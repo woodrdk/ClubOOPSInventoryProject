7e863ad47c9cf38b05322b427e8382de7cb90fda
Nubzoar <nubzoar@users.noreply.github.com>
2018-03-19 12:52:22 -0700

    Working on initial models.

---
+++ ClubOOPSInventoryProject/Models/InventoryItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClubOOPSInventoryProject.Models
+{
+    public class InventoryItem
+    {
+        [Key]
+        public int InventoryItemID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(400)]
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        public InventoryItemCondition Condition { get; set; }
+
+        /// <summary>
+        /// The User that an item is checked out to.
+        /// If NULL, the item is not checked out to anyone.
+        /// </summary>
+        public virtual ApplicationUser CheckedOutTo { get; set; }
+    }
+
+    public enum InventoryItemCondition
+    {
+        New,
+        Great,
+        Good,
+        Acceptable,
+        Damaged,
+        Broken
+    }
+}
